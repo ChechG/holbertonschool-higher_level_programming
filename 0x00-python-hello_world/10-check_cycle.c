c06b9a9da0fef7437e25a5eb3ef0241b616f8fc0
ChechG <2100@holbertonschool.com>
2020-12-07 18:26:24 +0000

    comment

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,22 +3,27 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * check_cycle - checks if there are cycles
+ * @list: linked list
+ * Return: 1 if cycle else 0.
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    if (list == NULL)
-    {
-        return (0);
-    }
-    while(list)
-    {
-        tmp = list;
-        list = list->next;
-        if (tmp <= list)
-        {
-            return (1);
-        }
-    }
-    return (0);
+	if (list == NULL)
+	{
+		return (0);
+	}
+	while (list)
+	{
+		tmp = list;
+		list = list->next;
+		if (tmp <= list)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
