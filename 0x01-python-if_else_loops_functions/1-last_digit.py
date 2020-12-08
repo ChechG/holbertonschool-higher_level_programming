102bc49c66f4bed154d0f05ad91128b4980eeaad
ChechG <2100@holbertonschool.com>
2020-12-08 16:01:35 +0000

    1 de nuevo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,24 +5,15 @@ number = random.randint(-10000, 10000)
 if number < 0:
     num = number * -1
     lastA = num % 10
-    if lastA > 5:
-        print("Last digit of {:d} ".format(number, end=""))
-        print("is {:d} and is greater than 5".format(lastA, end=""))
-    elif lastA == 0:
-        print("Last digit of {:d} ".format(number, end=""))
-        print("is {:d} and is 0".format(lastA, end=""))
-    elif lastA <= 5:
-        print("Last digit of {:d} ".format(lastA, end=""))
-        print("is {:d} and is less than 6 and not 0".format(lastA, end=""))
-
-if number >= 0:
-    lastB = number % 10
-    if lastB > 5:
-        print("Last digit of {:d} ".format(number, end=""))
-        print("is {:d} and is greater than 5".format(lastB, end=""))
-    elif lastB == 0:
-        print("Last digit of {:d} ".format(number, end=""))
-        print("is {:d} and is 0".format(lastB, end=""))
-    elif lastB <= 5:
-        print("Last digit of {:d} ".format(number, end=""))
-        print("is {:d} and is less than 6 and not 0".format(lastB, end=""))
+elif number > 0
+    lastA = num % 10
+if lastA > 5:
+    print("Last digit of {:d} ".format(number, end=""))
+    print("is {:d} and is greater than 5".format(lastA, end=""))
+elif lastA == 0:
+    print("Last digit of {:d} ".format(number, end=""))
+    print("is {:d} and is 0".format(lastA, end=""))
+elif lastA <= 5:
+    print("Last digit of {:d} ".format(lastA, end=""))
+    print("is {:d} and is less than 6 and not 0".format(lastA, end=""))
+    
