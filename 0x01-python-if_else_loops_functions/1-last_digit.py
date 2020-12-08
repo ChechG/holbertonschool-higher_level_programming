c19284bb1f583193fa3e25a6bcacb5322f0bec41
ChechG <2100@holbertonschool.com>
2020-12-08 15:58:42 +0000

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+
 if number < 0:
     num = number * -1
-    last = num % 10
-    if last > 5:
-        print("Last digit of {:d} is \
-        {:d} and is greater than 5".format(number, last))
-    elif last == 0:
-        print("Last digit of {:d} is \
-        {:d} and is 0".format(number, last))
-    elif last <= 5:
-        print("Last digit of {:d} is \
-        {:d} and is less than 6 and not 0".format(number, last))
+    lastA = num % 10
+    if lastA > 5:
+        print("Last digit of {:d} ".format(number, end=""))
+        print("is {:d} and is greater than 5".format(lastA, end=""))
+    elif lastA == 0:
+        print("Last digit of {:d} ".format(number, end=""))
+        print("is {:d} and is 0".format(lastA, end=""))
+    elif lastA <= 5:
+        print("Last digit of {:d} ".format(lastA, end=""))
+        print("is {:d} and is less than 6 and not 0".format(lastA, end=""))
 
 if number >= 0:
-    if last > 5:
-        print("Last digit of {:d} is \
-        {:d} and is greater than 5".format(number, last))
-    elif last == 0:
-        print("Last digit of {:d} is \
-        {:d} and is 0".format(number, last))
-    elif last <= 5:
-        print("Last digit of {:d} is \
-        {:d} and is less than 6 and not 0".format(number, last))
+    lastB = number % 10
+    if lastB > 5:
+        print("Last digit of {:d} ".format(number, end=""))
+        print("is {:d} and is greater than 5".format(lastB, end=""))
+    elif lastB == 0:
+        print("Last digit of {:d} ".format(number, end=""))
+        print("is {:d} and is 0".format(lastB, end=""))
+    elif lastB <= 5:
+        print("Last digit of {:d} ".format(number, end=""))
+        print("is {:d} and is less than 6 and not 0".format(lastB, end=""))
