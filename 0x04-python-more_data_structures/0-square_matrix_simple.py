b7a0e9da94eb2a02a35363a3ebc8d2d8c1d6c3f6
ChechG <2100@holbertonschool.com>
2020-12-17 15:57:11 +0000

    0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square = []
+    for i in matrix:
+        lista = []
+        square.append(lista)
+        for j in i:
+            lista.append(j ** 2)
+    return square
