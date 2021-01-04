17cb250fc1bd84ef07384e2c812b0ad0d4aaa5cd
ChechG <2100@holbertonschool.com>
2021-01-04 15:42:03 +0000

    pep8 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
-        print()
+        print("")
     except IndexError:
-        print()
+        print("")
         return i    
     return x
