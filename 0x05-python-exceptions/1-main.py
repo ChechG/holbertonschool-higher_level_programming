78c8465d4622862f36ac6efd8eeef30ea3af6eac
ChechG <2100@holbertonschool.com>
2021-01-04 15:57:24 +0000

    add task 1

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -2,6 +2,7 @@
 safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
 value = 89
+arr = [9]
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
@@ -15,3 +16,6 @@ value = "Holberton"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+has_been_print = safe_print_integer(arr)
+if not has_been_print:
+    print("{} is not an integer".format(arr))
