15cf1148a60bf4628ff864e0044bd69dc918861e
ChechG <2100@holbertonschool.com>
2021-01-04 20:19:21 +0000

    8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        res = fct(*args)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return
+    return res
