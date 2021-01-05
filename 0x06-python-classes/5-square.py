d7d7a0153787d26f8767276f550bfab288257e20
ChechG <2100@holbertonschool.com>
2021-01-05 19:28:55 +0000

    5 pep8

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,8 +27,9 @@ class Square:
     def area(self):
         """area"""
         return self.__size * self.__size
-    
+
     def my_print(self):
+        """print a square with #"""
         if self.__size > 0:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
