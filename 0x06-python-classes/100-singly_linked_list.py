21e5796e7a079adde0917f1e2c63ee415ca211fd
ChechG <2100@holbertonschool.com>
2021-01-05 23:16:59 +0000

    turb

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
-
+"""Square class"""
 
 class Node:
-  def __init__(self, data, next_node=None):
-    self.data = data
-    self.next_node = next_node
+    def __init__(self, data, next_node=None):
+        """Square class"""
+        self.data = data
+        self.next_node = next_node
+    
+    @property
+    def data(self):
+        """property to retrieve data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """property setter to set data"""
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """property to retrieve next node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """property to retrieve next node"""
+        self.__next_node = value
+
 
 class SinglyLinkedList:
+    """Square class"""
     def __init__(self):
         self.head = None
 
