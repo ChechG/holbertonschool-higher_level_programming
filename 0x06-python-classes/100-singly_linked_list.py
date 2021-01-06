799c8b00f1d5863eb47f7fd8bb82db571bb39931
ChechG <2100@holbertonschool.com>
2021-01-06 00:19:55 +0000

    re re RE turb

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,8 +5,6 @@
 class Node:
     def __init__(self, data, next_node=None):
         """Square class"""
-        if not type(data) is int:
-            raise TypeError("data must be an integer")
         self.data = data
         self.next_node = next_node
 
@@ -18,6 +16,8 @@ class Node:
     @data.setter
     def data(self, value):
         """property setter to set data"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
@@ -28,6 +28,8 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """property to retrieve next node"""
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
@@ -35,19 +37,19 @@ class SinglyLinkedList:
     """Square class"""
     def __init__(self):
         """Square class"""
-        self.head = None
+        self.__head = None
 
     def sorted_insert(self, value):
         """Square class"""
         n = Node(value)
-        if self.head is None:
+        if self.__head is None:
             n.next_node = None
-            self.head = n
-        elif self.head.data > value:
-            n.next_node = self.head
-            self.head = n
+            self.__head = n
+        elif self.__head.data > value:
+            n.next_node = self.__head
+            self.__head = n
         else:
-            curr = self.head
+            curr = self.__head
             while (curr.next_node is not None and curr.next_node.data < value):
                 curr = curr.next_node
             n.next_node = curr.next_node
@@ -56,7 +58,7 @@ class SinglyLinkedList:
     def __str__(self):
         """Square class"""
         datas = []
-        curr = self.head
+        curr = self.__head
         while curr is not None:
             datas.append(str(curr.data))
             curr = curr.next_node
