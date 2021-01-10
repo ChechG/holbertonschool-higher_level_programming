089b633f84dc529d905bfcdf269473ca369aac9e
ChechG <2100@holbertonschool.com>
2021-01-10 20:59:36 +0000

    final tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-Test of Module to find the max integer in a list
-"""
-
-
 import unittest
-from "6-max_integer.py" import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def 
+    def test_types(self):
+        self.assertRaises(TypeError, max_integer, [1, 2, 3, "hola"])
+        self.assertRaises(TypeError, max_integer, ["hola"], ["n"])
+
+    def test_equal(self):
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([123, 565, 3, -5647]), 565)
+        self.assertEqual(max_integer([-12, -23, -756, -2]), -2)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([0]), 0)
+
+    def test_empty(self):
+        empty_list = []
+        self.assertIsNone(max_integer(empty_list))
+        self.assertIsNone(max_integer())
+        self.assertRaises(TypeError, max_integer, None)
+
+if __name__ == "__main__":
+    unittest.main()
