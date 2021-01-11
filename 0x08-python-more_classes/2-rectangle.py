f3229b97a2998715c2c2c8805c555a65a2fdb4fd
ChechG <2100@holbertonschool.com>
2021-01-11 19:42:59 +0000

    pre checker

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by prior ej 1"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """__init__"""
+        if not type(width) is int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not type(height) is int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    def area(self):
+        """definition of area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """definition of perimeter"""
+        return (self.__width + self.__height) * 2
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by prior ej 3"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """__init__"""
+        if not type(width) is int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not type(height) is int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    def area(self):
+        """definition of area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """definition of perimeter"""
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """converts to string"""
+        rect = ""
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                rect += "#"
+            if i < self.__height - 1:
+                rect += "\n"
+        return rect
+
+    def __repr__(self):
+        """returns values"""
+        return("Rectangle({}, {})".format(self.width, self.height))
