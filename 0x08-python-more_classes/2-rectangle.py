3482939c27f9370e69469b3e6d1e12957df9f613
ChechG <2100@holbertonschool.com>
2021-01-11 21:59:40 +0000

    tiene que dar cero perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -55,4 +55,6 @@ class Rectangle:
 
     def perimeter(self):
         """definition of perimeter"""
+        if self.width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
