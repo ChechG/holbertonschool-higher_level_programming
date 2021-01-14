d63503510b5441ce136c42cfe4847bad930715bb
ChechG <2100@holbertonschool.com>
2021-01-14 13:43:10 +0000

    mistake

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Module to find the max integer in a list
+"""Doc
 """
 
 
 def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
+    """Doc
     """
     if len(list) == 0:
         return None
@@ -16,4 +14,5 @@ def max_integer(list=[]):
         if list[i] > result:
             result = list[i]
         i += 1
+    
     return result
--- 0x1A-hash_tables/1-djb2.c
+++
@@ -1,9 +0,0 @@
-#include "hash_tables.h"
-/**
- * hash_djb2 - hash function implementing the djb2 algorithm.
- * @str: string.
- * Return: unsigned long int.
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-}
