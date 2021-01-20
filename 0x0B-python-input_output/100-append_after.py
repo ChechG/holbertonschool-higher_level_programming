f690538e17d485b65d3b4b7ddd559f9e769e19cb
ChechG <2100@holbertonschool.com>
2021-01-20 23:33:50 +0000

    100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" append string after string """
+""" append string after a string """
 
 
 def append_after(filename="", search_string="", new_string=""):
     """ append string after string """
     with open(filename, mode='r') as my_file:
-        lines = my_file.readline()
+        lines = my_file.readlines()
+
     with open(filename, mode='w') as my_file:
         for i in lines:
             if search_string in i:
