a52bb306215924d5ba459364aa5ba2c3e4c861a3
ChechG <2100@holbertonschool.com>
2021-01-27 12:31:37 +0000

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,8 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """Function returns the JSON representation of an obj (str)"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
