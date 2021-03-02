07716903eb2488fee576f0497872b9eb4c0b948c
ChechG <2100@holbertonschool.com>
2021-03-02 19:57:33 +0000

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows WHERE tv_show_genres.genre_id >= 1 ORDER BY tv_shows.title, tv_show_genres.genre_id;
