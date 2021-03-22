f2d6b2f6a1d5076f6cd2b5b8917dd3d48c2892de
ChechG <2100@holbertonschool.com>
2021-03-22 20:33:11 +0000

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const num = process.argv[2];
+console.log(factorial(num));
+function factorial (a) {
+  if (a == null) {
+    return 1;
+  }
+  if (a === 0) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
