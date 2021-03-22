7d32d6a7af986f0af2e36c796ff350bb10b8fd11
ChechG <2100@holbertonschool.com>
2021-03-22 18:41:55 +0000

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No arguments found');
-} else if (process.argv.length === 3) {
-  console.log('Agument found');
-} else if (process.argv.length > 3) {
+if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
   console.log('Aguments found');
 }
