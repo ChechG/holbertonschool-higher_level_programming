d3da3e2900c3a9e259fd5604df8497adbcba3acd
ChechG <2100@holbertonschool.com>
2021-03-22 18:09:16 +0000

    carpeta

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('No arguments found');
+} else if (process.argv.length === 3) {
+  console.log('Agument found');
+} else if (process.argv.length > 3) {
+  console.log('Aguments found');
+}
