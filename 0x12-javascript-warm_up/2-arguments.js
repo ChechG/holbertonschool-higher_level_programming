0e2cb52f9f880f85ec84e0dee3f12e6c1664af58
ChechG <2100@holbertonschool.com>
2021-03-22 18:43:20 +0000

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,5 +4,5 @@ if (process.argv.length === 3) {
 } else if (process.argv.length === 2) {
   console.log('No argument');
 } else {
-  console.log('Aguments found');
+  console.log('Arguments found');
 }
