3dac448761a02365c651eb8440bd49569f493c47
ChechG <2100@holbertonschool.com>
2021-03-22 21:30:12 +0000

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,11 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log('0');
 } else {
-  let nums = process.argv.slice(2);
-  let arrnums = [];
+  const nums = process.argv.slice(2);
+  const arrnums = [];
   for (let i = 0; i < nums.length; i++) {
     arrnums.push(parseInt(nums[i]));
   }
-  arrnums.sort(function(a, b){return a-b});
+  arrnums.sort(function (a, b) { return a - b; });
   console.log(arrnums[arrnums.length - 2]);
 }
