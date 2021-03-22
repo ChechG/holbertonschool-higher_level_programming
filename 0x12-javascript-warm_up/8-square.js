3d6267bdc8fa7df63cc3fe21d664a3596ae00c14
ChechG <2100@holbertonschool.com>
2021-03-22 19:45:07 +0000

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing size');
+} else if (parseInt(process.argv[2]) > 0) {
+  let x = '';
+  for (let i = 0; i < (parseInt(process.argv[2])); i++) {
+    for (let j = 0; j < (parseInt(process.argv[2])); j++) {
+      x += 'X';
+    }
+    if (i < (parseInt(process.argv[2]) - 1)) {
+      x += '\n';
+    }
+  }
+  console.log(x);
+}
