90a0eb83a2cf9b094985eb53315f93bf625cd647
ChechG <2100@holbertonschool.com>
2021-03-22 20:03:14 +0000

    9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+console.log(add(process.argv[2], process.argv[3]));
+function add (a, b) {
+  if (isNaN(parseInt(a)) || isNaN(parseInt(b))) {
+    return NaN;
+  }
+  return parseInt(a) + parseInt(b);
+}
