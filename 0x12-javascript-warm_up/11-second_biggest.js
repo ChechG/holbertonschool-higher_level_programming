0fda22e24b337bf41d6150655a9a35ea9d87c766
ChechG <2100@holbertonschool.com>
2021-03-22 20:55:30 +0000

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const nums = process.argv.slice(2);
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log('0');
+} else {
+  nums.sort();
+  console.log(nums[nums.length - 2]);
+}
