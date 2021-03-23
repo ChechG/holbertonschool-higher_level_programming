6db49fe8279027dd6eafba5883c453c70acd1b75
ChechG <2100@holbertonschool.com>
2021-03-23 17:30:46 +0000

    nul

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,4 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let rect = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        rect += 'X';
-      }
-      if (i < this.height - 1) {
-        rect += '\n';
-      }
-    }
-    console.log(rect);
-  }
-
-  rotate () {
-    const curr = this.height;
-    this.height = this.width;
-    this.width = curr;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-module.exports = Rectangle;
-
 const Squareone = require('./5-square');
 
 class Square extends Squareone {
@@ -42,7 +8,7 @@ class Square extends Squareone {
   }
 
   charPrint (c) {
-    if (c == null) {
+    if (c === undefined) {
       c = 'X';
     }
     let printR = '';
