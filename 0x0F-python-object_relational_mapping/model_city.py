9a2cc92f53eedcd01686c66e2501c2793dc37ba3
ChechG <2100@holbertonschool.com>
2021-03-27 12:29:02 +0000

    model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+""" comentario """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class City(Base):
+    """ comentario """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
