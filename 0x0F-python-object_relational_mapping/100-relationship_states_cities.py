69c357bc4e5bc611299ada0224a23f628186809c
ChechG <2100@holbertonschool.com>
2021-03-29 00:49:13 +0000

    100 2

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,13 +11,15 @@ import sqlalchemy
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]), pool_pre_ping="True")
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping="True")
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    row = State(name="California")
-    session.add(row)
-    session.commit()
+    new_state = State(name="California")
     new_city = City(name="San Francisco")
-    row.cities.append(new_city)
+    new_state.cities.append(new_city)
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
     session.close()
