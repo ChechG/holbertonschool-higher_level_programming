0dc2d8944a13a17521c1482d1c924604862cdad5
ChechG <2100@holbertonschool.com>
2021-04-08 22:14:29 +0000

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # JSON POST displays the body of the response.
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @$2 $1
+curl -H "Content-type: application/json" -X POST -d @$2 $1
