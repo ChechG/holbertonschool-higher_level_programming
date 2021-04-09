de4e544f271666697ec2c50b08e4596291a071de
ChechG <2100@holbertonschool.com>
2021-04-09 18:45:53 +0000

    3 lacked with

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,5 +12,8 @@ if __name__ == "__main__":
     req = urllib.request.Request(argv[1])
     try:
         urllib.request.urlopen(req)
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
     except urllib.error.URLError as e:
         print("Error code: {}".format(e.code))
