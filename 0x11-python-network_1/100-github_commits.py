02fa7b31b9c84f65538e29756a8e88a73ca996c2
ChechG <2100@holbertonschool.com>
2021-04-09 21:21:57 +0000

    100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  Script that takes 2 arguments in order to solve a challenge. """
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    owner = argv[1]
+    repo = argv[2]
+    u = 'https://api.github.com/repos/' + owner + "/" + repo + "/commits"
+    url = requests.get(u).json()
+    cont = 0
+    for key in url:
+        if cont < 10:
+            print("{}: {}".format(key['sha'], key['commit']['author']['name']))
+            cont += 1
+        else:
+            break
