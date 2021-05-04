aea37bad1cdd06f7a95c5c6f093b0189f9abeb03
ChechG <2100@holbertonschool.com>
2021-05-04 22:51:11 +0000

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,20 +9,14 @@ request(webP, function (err, res, body) {
   } else {
     const dict = {};
     let i;
-    let nId = 1;
     const tareas = JSON.parse(body);
     const largo = tareas.length;
     for (i = 0; i < largo; i++) {
       if (nId === tareas[i].userId) {
-        if (tareas[i].completed === true) {
-          if (!(tareas[i].userId in dict)) {
-            dict[tareas[i].userId] = 0;
-          }
-          dict[tareas[i].userId] += 1;
+        if (!(tareas[i].userId in dict)) {
+          dict[tareas[i].userId] = 0;
         }
-      } else {
-        nId += 1;
-        i -= 1;
+        dict[tareas[i].userId] += 1;
       }
     }
     console.log(dict);
