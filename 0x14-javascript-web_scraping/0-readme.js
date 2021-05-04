f907a2b0f987555d84fb4348154b1405dba16a0f
ChechG <2100@holbertonschool.com>
2021-05-04 15:19:22 +0000

    1 semistandard again

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const arg1 = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(arg1, 'utf8' , (err, data) => {
+fs.readFile(arg1, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
   console.log(data);
-})
+});
