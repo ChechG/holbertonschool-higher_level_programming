b120f2926e77dc3cc65d42c46f4bde0c7513b41a
ChechG <2100@holbertonschool.com>
2021-05-05 00:55:28 +0000

    101 fail

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,15 +9,23 @@ request(req, function (err, res, body) {
     console.logr(err);
   } else {
     let i;
+    let num = 1;
+    const personaje = JSON.parse(body).characters;
     for (i = 0; i < JSON.parse(body).characters.length; i++) {
-      const personaje = JSON.parse(body).characters[i];
-      request(personaje, function (error, resp, bdy) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(bdy).name);
-        }
-      });
+      const per = 'https://swapi-api.hbtn.io/api/people/' + num + '/';
+      if (personaje.includes(per)) {
+        request(per, function (error, resp, bdy) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(JSON.parse(bdy).name);
+          }
+        });
+        num += 1;
+      } else {
+        num += 1;
+        i -= 1;
+      }
     }
   }
 });
