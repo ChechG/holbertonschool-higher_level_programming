309b493a6d1b1dbc5920ebc49423c02613d9e4dd
ChechG <2100@holbertonschool.com>
2021-05-05 00:22:50 +0000

    101 sin ordenar

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const req = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(req, function (err, res, body) {
+  if (err) {
+    console.logr(err);
+  } else {
+    let i;
+    for (i = 0; i < JSON.parse(body).characters.length; i++) {
+      const personaje = JSON.parse(body).characters[i];
+      request(personaje, function (error, resp, bdy) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(bdy).name);
+        }
+      });
+    }
+  }
+});
